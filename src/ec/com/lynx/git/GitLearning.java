ab343489203d044d8302a83d8cee690f6a1e9b5f
Diego Chacon <dchacon@lynx.com.ec>
2016-11-21 11:49:31 -0500

    This is just the first test

---
+++ src/ec/com/lynx/git/GitLearning.java
@@ -0,0 +1,11 @@
+package ec.com.lynx.git;
+
+public class GitLearning {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is just a test 1");
+		System.out.println("This is just a test 2");
+	}
+
+}
