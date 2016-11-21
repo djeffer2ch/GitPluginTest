90ca1fefb33c5cc90040132faada8eb6c4f07d42
Diego Jefferson <dchacon@lynx.com.ec>
2016-11-21 12:01:32 -0500

    Branch review

--- src/ec/com/lynx/git/GitLearning.java
+++ src/ec/com/lynx/git/GitLearning.java
@@ -4,9 +4,7 @@ public class GitLearning {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is just a test 1");
-		System.out.println("This is just a test 2");
-		System.out.println("This is another test 3");
+		System.out.println("Hello Git, from branch bug#1234");
 	}
 
 }
